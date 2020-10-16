3929e9b0ab665bfdb5edb07837d76c2f5fb5d0cc
Tristan Eastman <teastman85@gmail.com>
2020-10-16 07:13:12 -0400

    Add SQL database and seeds as well as config connection.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Vegatable Burger");
+INSERT INTO burgers (burger_name) VALUES ("Meatless Taco Burger");
+INSERT INTO burgers (burger_name) VALUES ("Impossible Burger");
